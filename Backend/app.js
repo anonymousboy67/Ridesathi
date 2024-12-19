f5bf6ec638bb63607717a128b313feb0b94edaab
anonymousboy67 <aashishad67@gmail.com>
2024-12-19 12:11:42 +0545

    message

--- Backend/app.js
+++ Backend/app.js
@@ -1,22 +1,27 @@
 const dotenv = require('dotenv');
 dotenv.config();
-const express=require('express')
-const cors=require('cors');
-const app=express();
+
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser'); // Corrected
+
+const app = express();
 const userRoutes = require('./routes/user.routes.js');
+const connectToDatabase = require('./db/db');
 
-const connectToDatabase=require('./db/db')
-
+// Connect to the database
 connectToDatabase();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-app.get('/', (req,res)=>{
-    res.send('Hello World')
-})
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
 
+// User routes
 app.use('/users', userRoutes);
 
-module.exports=app;
+module.exports = app;
